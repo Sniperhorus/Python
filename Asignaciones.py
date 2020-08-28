b00417c9b2e701bab8ec1d7a79a0e348bd152f36
Sniperhorus <carrilloimauricio@gamil.com>
2020-08-27 22:03:40 -0500

    Asignaciones y palabras reservadas

---
+++ Asignaciones.py
@@ -0,0 +1,16 @@
+# Puedes asignar valores a cada variable linea por linea.
+prueba1 = 10
+prueba2 = "Mauricio"
+prueba3 = 10*5
+
+print(prueba1)
+print(prueba2)
+print(prueba3)
+
+# O utilizar la asignacion de manera lineal.
+# Solo hay que tener en cuenta que debe ser legible y no declarar muchas variables en 1 sola linea
+test1, test2, test3 = 1, "Diego", 45+2
+
+print(test1)
+print(test2)
+print(test3)
