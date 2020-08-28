8f9a70af3d02236b4e64cb48990cd4bd81e36376
Sniperhorus <carrilloimauricio@gamil.com>
2020-08-27 21:55:55 -0500

    Variables y constantes

---
+++ Variables.py
@@ -0,0 +1,9 @@
+# Declarion de variables , si el nombre es mas de 1 palabra utilizar snake_case e.g -> nombre_tutor
+tutor = "Mauricio"
+print (tutor)
+
+# Una constante es una variable que no cambiara de valor en la ejecucion.
+# Debe ser escrita en mayusculas CONSTANTE="soy un constante"
+
+CONSTANTE="TEST"
+print(CONSTANTE)
