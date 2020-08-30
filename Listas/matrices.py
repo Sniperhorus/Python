f3789be8ef870439c3e214e906d14680f70ce231
Mauricio <carrilloimauricio15@gmail.com>
2020-08-30 02:33:22 -0500

    Matrices

---
+++ Listas/matrices.py
@@ -0,0 +1,6 @@
+list1 = [20,30]
+list2 = [40,50]
+# Una matriz es anidar listas dentro de otra lista
+matrix = [list1,list2]
+# Para tratarla como matriz y acceder a cada uno de sus valores se declaran de la siguiente forma (x,y)
+print(matrix[1][1])
