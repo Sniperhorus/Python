71c2da3eefdb5197c259a95a1501da5919657d8b
Mauricio <carrilloimauricio15@gmail.com>
2020-09-02 22:47:39 -0500

    String overview

---
+++ Strings/secuencia.py
@@ -0,0 +1,11 @@
+# Un string que ya fue declardo, es inmutable es decir sus valores no pueden
+# Ser modificados
+# Podemos acceder a los valores del string mediante indices, generar limites y saltos
+#
+#
+#
+curso = 'Esto es una cadena de texto'
+
+print(len(curso))
+
+print(curso[0],curso[-1])
